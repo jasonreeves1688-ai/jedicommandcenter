068c739090d97d08241daf7e651b0bce76fbdf9b
jasonreeves1688-ai <jason.reeves1688@gmail.com>
2026-05-23 20:23:10 -0500

    Delete games/snake directory

--- games/snake/reeves-bridge.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  'use strict';
-  function post(type,score){
-    try{ window.parent.postMessage({type:type,score:Number(score)||0},'*'); }catch(e){}
-  }
-  window.ReevesScore    = function(s){ post('REEVES_SCORE',s); };
-  window.ReevesGameOver = function(s){ post('REEVES_GAME_OVER',s); };
-  console.log('[Reeves Bridge] Loaded.');
-})();
